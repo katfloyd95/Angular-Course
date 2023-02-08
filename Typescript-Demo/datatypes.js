71fc427f8dc9e14bb16549b877704f0d011b33ce
Katarina <katarina.floyd1@gmail.com>
2023-02-08 10:12:12 +0100

    add data type examples

--- Typescript-Demo/datatypes.js
+++ Typescript-Demo/datatypes.js
@@ -1,3 +1,29 @@
 "use strict";
-let lName = "Smith";
-lName = 10;
+let lName;
+lName = "Floyd";
+let newName = lName.toUpperCase();
+console.log(newName);
+let age;
+age = 27;
+age = 27.5;
+let dob = "27";
+let result = parseInt(dob);
+let isValid = true;
+console.log(isValid);
+let empList; //  assign string rule to array
+empList = ["Katarina", "Floyd", "Henderson"];
+let numList; // another way to assign rule to array
+numList = [1, 2, 3, 4, 5];
+let filteredResult = numList.filter((num) => num > 2);
+console.log(filteredResult);
+let foundNumber = numList.find((num) => num === 2);
+console.log(foundNumber);
+let c = 1 /* Color.Blue */;
+let swapNums;
+function swapNumbers(num1, num2) {
+    return [num2, num1];
+}
+swapNums = swapNumbers(10, 20);
+console.log(swapNums);
+let department;
+// let department; // implicit any
--- Typescript-Demo/datatypes.ts
+++ Typescript-Demo/datatypes.ts
@@ -1,2 +1,46 @@
-let lName = "Smith";
-lName = 10;
+let lName: string;
+lName = "Floyd";
+
+let newName = lName.toUpperCase();
+console.log(newName);
+
+let age: number;
+age = 27;
+age = 27.5;
+
+let dob = "27";
+
+let result = parseInt(dob);
+
+let isValid: boolean = true;
+console.log(isValid);
+
+let empList: string[]; //  assign string rule to array
+empList = ["Katarina", "Floyd", "Henderson"];
+
+let numList: Array<number>; // another way to assign rule to array
+numList = [1, 2, 3, 4, 5];
+
+let filteredResult = numList.filter((num) => num > 2);
+console.log(filteredResult);
+
+let foundNumber = numList.find((num) => num === 2);
+console.log(foundNumber);
+
+const enum Color {
+  Red,
+  Blue,
+  Green,
+}
+
+let c: Color = Color.Blue;
+
+let swapNums: [firstNumber: number, secondNumber: number];
+function swapNumbers(num1: number, num2: number): [number, number] {
+  return [num2, num1];
+}
+swapNums = swapNumbers(10, 20);
+console.log(swapNums);
+
+let department: any;
+// let department; // implicit any
