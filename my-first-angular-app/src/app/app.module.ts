352762ac8fc44ece39e54d77373d9fd9b4315954
Katarina <katarina.floyd1@gmail.com>
2023-02-14 17:23:35 +0100

    basics assignment 1

--- my-first-angular-app/src/app/app.module.ts
+++ my-first-angular-app/src/app/app.module.ts
@@ -3,9 +3,11 @@ import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
+import { WarningComponent } from './warning/warning.component';
+import { SuccessComponent } from './success/success.component';
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [AppComponent, WarningComponent, SuccessComponent],
   imports: [BrowserModule, FormsModule],
   providers: [],
   bootstrap: [AppComponent],
