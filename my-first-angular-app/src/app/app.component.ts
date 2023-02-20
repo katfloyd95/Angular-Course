0340ae7dc582b7c0a6692837157d7047d2961ebb
Katarina <katarina.floyd1@gmail.com>
2023-02-20 10:34:21 +0100

    Custom Property Binding

--- my-first-angular-app/src/app/app.component.ts
+++ my-first-angular-app/src/app/app.component.ts
@@ -6,5 +6,11 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  serverElements: { type: string; name: string; content: string }[] = [];
+  serverElements = [
+    {
+      type: 'server',
+      name: 'Test Server',
+      content: 'Just a test!',
+    },
+  ];
 }
