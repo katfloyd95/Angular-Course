d95deccaf84860224d2c31d0e1058c99bfe3d624
Katarina <katarina.floyd1@gmail.com>
2023-02-21 14:19:16 +0100

    reset code for assignment

--- my-first-angular-app/src/app/app.component.ts
+++ my-first-angular-app/src/app/app.component.ts
@@ -5,39 +5,4 @@ import { Component } from '@angular/core';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
 })
-export class AppComponent {
-  serverElements = [
-    {
-      type: 'server',
-      name: 'Test Server',
-      content: 'Just a test!',
-    },
-  ];
-
-  onServerAdded(serverData: { serverName: string; serverContent: string }) {
-    this.serverElements.push({
-      type: 'server',
-      name: serverData.serverName,
-      content: serverData.serverContent,
-    });
-  }
-
-  onBlueprintAdded(blueprintData: {
-    blueprintName: string;
-    blueprintContent: string;
-  }) {
-    this.serverElements.push({
-      type: 'blueprint',
-      name: blueprintData.blueprintName,
-      content: blueprintData.blueprintContent,
-    });
-  }
-
-  OnChangeFirst() {
-    this.serverElements[0].name = 'Changed!';
-  }
-
-  OnDestroyFirst() {
-    this.serverElements.splice(0, 1);
-  }
-}
+export class AppComponent {}
--- my-first-angular-app/src/app/app.module.ts
+++ my-first-angular-app/src/app/app.module.ts
@@ -3,11 +3,9 @@ import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
-import { CockpitComponent } from './cockpit/cockpit.component';
-import { ServerElementComponent } from './server-element/server-element.component';
 
 @NgModule({
-  declarations: [AppComponent, CockpitComponent, ServerElementComponent],
+  declarations: [AppComponent],
   imports: [BrowserModule, FormsModule],
   providers: [],
   bootstrap: [AppComponent],
