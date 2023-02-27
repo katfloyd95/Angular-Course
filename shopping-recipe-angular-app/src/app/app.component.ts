9d456b29abda57c155b3d6c87b946d6f554a5b6e
katfloyd95 <katarina.floyd1@gmail.com>
2023-02-27 12:29:23 +0100

    course project: add nav directory function

--- shopping-recipe-angular-app/src/app/app.component.ts
+++ shopping-recipe-angular-app/src/app/app.component.ts
@@ -7,4 +7,8 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'shopping-recipe-angular-app';
+  loadedFeature: string = 'recipe';
+  onNavigate(feature: string) {
+    this.loadedFeature = feature;
+  }
 }
