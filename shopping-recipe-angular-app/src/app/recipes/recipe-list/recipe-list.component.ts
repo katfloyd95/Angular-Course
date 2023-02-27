8cce556df4be4008765aff499d9ce7cfeaf2364e
katfloyd95 <katarina.floyd1@gmail.com>
2023-02-27 16:54:07 +0100

    recipe details display wip

--- shopping-recipe-angular-app/src/app/recipes/recipe-list/recipe-list.component.ts
+++ shopping-recipe-angular-app/src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import {Component, Output, EventEmitter} from '@angular/core';
 import { Recipe } from './recipe.model';
 
 @Component({
@@ -7,11 +7,21 @@ import { Recipe } from './recipe.model';
   styleUrls: ['./recipe-list.component.css'],
 })
 export class RecipeListComponent {
+  @Output() recipeWasSelected = new EventEmitter<Recipe>();
   recipes: Recipe[] = [
     new Recipe(
       'Test Recipe',
       'This is simply a test',
       'https://images.immediate.co.uk/production/volatile/sites/30/2020/08/chorizo-mozarella-gnocchi-bake-cropped-9ab73a3.jpg'
     ),
+    new Recipe(
+      'Test Recipe',
+      'This is simply a test',
+      'https://images.immediate.co.uk/production/volatile/sites/30/2020/08/chorizo-mozarella-gnocchi-bake-cropped-9ab73a3.jpg'
+    ),
   ];
+
+  onRecipeSelected(recipe: Recipe) {
+    this.recipeWasSelected.emit(recipe);
+  }
 }
