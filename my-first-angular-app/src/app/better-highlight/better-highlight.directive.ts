45d6b87b6a955a7d7e7c321aecdaad156e57d547
katfloyd95 <katarina.floyd1@gmail.com>
2023-08-24 09:58:38 +0200

    Binding to Directive Properties

--- my-first-angular-app/src/app/better-highlight/better-highlight.directive.ts
+++ my-first-angular-app/src/app/better-highlight/better-highlight.directive.ts
@@ -1,4 +1,4 @@
-import {Directive, HostListener, HostBinding, Input, OnInit} from '@angular/core';
+import {Directive, HostListener, HostBinding, Input} from '@angular/core';
 
 @Directive({
   selector: '[appBetterHighlight]'
