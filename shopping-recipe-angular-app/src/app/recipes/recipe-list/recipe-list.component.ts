86088ea962e2876c427fe59ca0857c185641c49f
katfloyd95 <katarina.floyd1@gmail.com>
2024-02-11 15:21:29 +0100

    Add recipe and shopping list services & set up recipe service

--- shopping-recipe-angular-app/src/app/recipes/recipe-list/recipe-list.component.ts
+++ shopping-recipe-angular-app/src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,25 +1,22 @@
 import {Component, Output, EventEmitter} from '@angular/core';
 import { Recipe } from './recipe.model';
+import {RecipesService} from "../recipes.service";
+import {OnInit} from "@angular/core";
 
 @Component({
   selector: 'app-recipe-list',
   templateUrl: './recipe-list.component.html',
-  styleUrls: ['./recipe-list.component.css'],
+  styleUrls: ['./recipe-list.component.css']
 })
-export class RecipeListComponent {
+export class RecipeListComponent implements OnInit {
   @Output() recipeWasSelected = new EventEmitter<Recipe>();
-  recipes: Recipe[] = [
-    new Recipe(
-      'Test Recipe',
-      'This is simply a test',
-      'https://images.immediate.co.uk/production/volatile/sites/30/2020/08/chorizo-mozarella-gnocchi-bake-cropped-9ab73a3.jpg'
-    ),
-    new Recipe(
-      'Another Test Recipe',
-      'This is simply a test',
-      'https://images.immediate.co.uk/production/volatile/sites/30/2020/08/chorizo-mozarella-gnocchi-bake-cropped-9ab73a3.jpg'
-    ),
-  ];
+  recipes: Recipe[] | undefined;
+
+  constructor(private recipesService: RecipesService) {}
+
+  ngOnInit() {
+    this.recipes = this.recipesService.getRecipes();
+  }
 
   onRecipeSelected(recipe: Recipe) {
     this.recipeWasSelected.emit(recipe);
---
+++ shopping-recipe-angular-app/src/app/recipes/recipes.service.ts
@@ -0,0 +1,20 @@
+import {Recipe} from "./recipe-list/recipe.model";
+
+export class RecipesService {
+  private recipes: Recipe[] = [
+    new Recipe(
+      'Test Recipe',
+      'This is simply a test',
+      'https://images.immediate.co.uk/production/volatile/sites/30/2020/08/chorizo-mozarella-gnocchi-bake-cropped-9ab73a3.jpg'
+    ),
+    new Recipe(
+      'Another Test Recipe',
+      'This is simply a test',
+      'https://images.immediate.co.uk/production/volatile/sites/30/2020/08/chorizo-mozarella-gnocchi-bake-cropped-9ab73a3.jpg'
+    ),
+  ];
+
+  getRecipes() {
+    return this.recipes.slice();
+  }
+}
